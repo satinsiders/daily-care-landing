b47382fe57cd15a05b0ba253828bf0b5ba8e8837
satinsiders <satinsiders@gmail.com>
2025-06-21 15:59:43 +0900

    Add Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(
+              function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);}
+            )(window,document,'script','dataLayer','GTM-KNZTP9TB');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
@@ -10,6 +19,16 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="font-body text-gray-800">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KNZTP9TB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
