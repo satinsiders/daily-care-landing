ab214658239bfb536afc290d8d04b44c27a73771
satinsiders <satinsiders@gmail.com>
2025-06-23 16:01:02 +0900

    Update index.js with LeadForm show

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,7 @@ export default function Home() {
         <HowItWorks />
         <Features />
         <Pricing /> 
+        <LeadForm />
         <Curriculum />
         <ComparisonTable />
         <Testimonials />
