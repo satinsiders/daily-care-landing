a656bbe2cfffd196cb3740aea12365616fafde13
satinsiders <satinsiders@gmail.com>
2025-07-09 14:56:52 +0900

    Add sticky promo footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import "../styles/globals.css";
+import StickyFooter from "../components/StickyFooter";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Component {...pageProps} />
+      <StickyFooter />
+    </>
+  );}
