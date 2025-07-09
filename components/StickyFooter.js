43edb3b5a9a9c70e5d9d7a1b380b044f7a9fbb17
satinsiders <satinsiders@gmail.com>
2025-07-09 15:19:25 +0900

    Update StickyFooter.js

--- components/StickyFooter.js
+++ components/StickyFooter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function StickyFooter() {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-accent text-white text-center py-3 z-50">
+    <div className="fixed bottom-0 left-0 w-full bg-accent text-white text-center py-3 z-50 font-bold">
       🎯 100% Free August SAT Prep – Only 100 Spots!
     </div>
   );
